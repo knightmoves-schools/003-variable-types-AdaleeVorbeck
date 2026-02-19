d3206140eb1b0e8a08fe55a9791adf2f48b3f145
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-02-19 20:02:39 +0000

    THEN I REALIZE

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -2,4 +2,10 @@
 public class VariableTypes
 {   
     // Add your code here
+    public string AnimalName = "Maned Wolf";
+    public char GradeLetter = 'A';
+    public int Year = 2036;
+    public double Hight = 5.2;
+    public bool IsIndoorPet = false;
+
 }
